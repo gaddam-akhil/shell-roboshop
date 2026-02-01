4bb404530fbe3951c285074fa0c7a89b141007dc
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-01 17:54:13 +0530

    roboshop mongodb instance

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 SG_ID="sg-0fd4180ae5446c35c"
 AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z02323877XG4RCKJ3YPJ"
+DOMAIN_NAME="gaddam.online"
 
 for INSTANCE in $@
 do
@@ -15,18 +17,46 @@ InstanceId=$( aws ec2 run-instances \
 
 if [ $INSTANCE == "frontend" ]; then
   IP=$(
-       aws ec2 describe-instances \
+     aws ec2 describe-instances \
      --instance-ids $InstanceId \
      --query 'Reservations[*].Instances[*].PublicIpAddress' \
      --output text
  )
+ RECORD_NAME="$DOMAIN_NAME"
 else
     IP=$(
-        aws ec2 describe-instances \
+     aws ec2 describe-instances \
      --instance-ids $InstanceId \
      --query 'Reservations[*].Instances[*].PrivateIpAddress' \
      --output text
     )
+    RECORD_NAME="$INSTANCE.$DOMAIN_NAME"
  fi
  echo " IP ADDRESS - $IP"
+
+ aws route53 change-resource-record-sets \
+ --hosted-zone-id $ZONE_ID 
+ --change-batch '
+ {
+  "Comment": "Updating record",
+  "Changes": [
+    {
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "'$RECORD_NAME'",
+        "Type": "A",
+        "TTL": 1,
+        "ResourceRecords": [
+          {
+            "Value": "'$IP'"
+          }
+        ]
+      }
+    }
+  ]
+}
+
+'
+echo "record updated for $INSTANCE"
+
 done
